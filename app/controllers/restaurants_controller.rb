c20ca93bf256896e7ddec1badf3704092d2791da
Stallone Dias <stalloneds@gmail.com>
2019-05-21 15:09:17 +0000

    added user reg

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,4 +3,20 @@ class RestaurantsController < ApplicationController
 	def index
 		@restaurants = Restaurant.all
 	end 
+
+	def new
+		@restaurant = Restaurant.new
+	end 
+
+	def create
+		Restaurant.create(restaurant_params)
+		redirect_to root_path
+	end 
+
+private 
+
+	def restaurant_params
+		params.require(:restaurant).permit(:name, :address, :phone, :description)
+	end 
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,8 @@
 Rails.application.configure do
+
+  # added for devise gem
+   config.action_mailer.default_url_options = { host: 'yummy-stallone-dias.herokuapp.com' }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
